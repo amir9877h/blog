da9ebd0fd12c43523a32d38cb78554ce8ddbf97c
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-05-01 20:00:47 +0330

    making SplashScreen

---
+++ lib/splash.dart
@@ -0,0 +1,38 @@
+import 'package:blog/gen/assets.gen.dart';
+import 'package:blog/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
+        return HomeScreen();
+      }));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+              child: Assets.img.background.splash.image(fit: BoxFit.cover)),
+          Center(
+            child: Assets.img.icons.logo.svg(width: 100),
+          )
+        ],
+      ),
+    );
+  }
+}
